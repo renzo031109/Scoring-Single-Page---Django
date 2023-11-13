1d786e18ed27b2326d6382782d2591f907015cdf
Renan Rivera <renzo031109@gmail.com>
2023-11-13 15:54:24 +0800

    add function

--- score/views.py
+++ score/views.py
@@ -3,11 +3,23 @@ from .models import Score
 from .forms import ScoreForm
 
 def home(request):
-    form = ScoreForm()
     title = 'Home'
     scores = Score.objects.all()
+    form = ScoreForm()
+    if request.method == 'POST':
+        if 'save' in request.POST:
+            form = ScoreForm(request.POST)
+            form.save()
+        
+        elif 'delete' in request.POST:
+            pk = request.POST.get('delete')
+            score = Score.objects.get(id=pk)
+            score.delete()
+
     return render(request,'score/home.html', {
         'scores': scores,
         'title': title,
         'form': form
     })
+
+
