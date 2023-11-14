5c27726ce2d4c73addc08d2b18d06b7803d4ffdd
Renan Rivera <renzo031109@gmail.com>
2023-11-14 09:37:11 +0800

    fixed error edit and delete

--- score/views.py
+++ score/views.py
@@ -3,23 +3,29 @@ from .models import Score
 from .forms import ScoreForm
 
 def home(request):
-    title = 'Home'
-    scores = Score.objects.all()
     form = ScoreForm()
+    scores = Score.objects.all()
+    title = 'Home'
     if request.method == 'POST':
         if 'save' in request.POST:
-            form = ScoreForm(request.POST)
+            pk = request.POST.get('save')
+            if not pk:
+                form = ScoreForm(request.POST)
+            else:
+                score = Score.objects.get(id=pk)
+                form = ScoreForm(request.POST, instance=score)
             form.save()
-        
+            form = ScoreForm()
         elif 'delete' in request.POST:
             pk = request.POST.get('delete')
             score = Score.objects.get(id=pk)
             score.delete()
-
-    return render(request,'score/home.html', {
-        'scores': scores,
+        elif 'edit' in request.POST:
+            pk = request.POST.get('edit')
+            score = Score.objects.get(id=pk)
+            form = ScoreForm(instance=score)
+    return render(request, 'score/home.html', {
+        'form': form,
         'title': title,
-        'form': form
+        'scores': scores
     })
-
-
